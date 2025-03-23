39c4959c757de1681f280fa7f6dc8d6dd5b69940
joaovictorsenna <victor.sena12@hotmail.com>
2025-03-22 23:04:33 -0300

    Devburger-API

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+
+import configDatabase from "../config/database";
+
+import User from "../app/models/User";
+
+class Database {
+    constructor() {
+        this.init()
+    }
+
+    init() {
+        this.connection = new Sequelize(configDatabase)
+        model.map((model) => model.init(this.connection))
+    }
+}
+export default new Database()
